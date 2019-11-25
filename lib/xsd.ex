d8fd4e47672f5666bb666b419ffdc73075d80e01
Marcel Otto <marcelotto@gmx.de>
2019-11-26 00:46:48 +0100

    Initial commit
    	$ mix new xsd --module XSD
    
    on Elixir 1.9.4

---
+++ lib/xsd.ex
@@ -0,0 +1,18 @@
+defmodule XSD do
+  @moduledoc """
+  Documentation for XSD.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XSD.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xsd_test.exs
@@ -0,0 +1,8 @@
+defmodule XSDTest do
+  use ExUnit.Case
+  doctest XSD
+
+  test "greets the world" do
+    assert XSD.hello() == :world
+  end
+end
