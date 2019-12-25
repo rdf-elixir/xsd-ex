13cf52b1bef0b41e4a16028167c07a2c05435b4a
Marcel Otto <marcelotto@gmx.de>
2019-12-25 02:32:57 +0100

    Add basic boolean functions

--- lib/xsd/datatypes/boolean.ex
+++ lib/xsd/datatypes/boolean.ex
@@ -49,6 +49,173 @@ defmodule XSD.Boolean do
       @invalid_value
     end
   end
+
+  @doc """
+  Returns an Effective Boolean Value (EBV).
+
+  The Effective Boolean Value is an algorithm to coerce values to a `XSD.Boolean`.
+
+  It is specified and used in the SPARQL query language and is based upon XPath's
+  `fn:boolean`. Other than specified in these specs any value which can not be
+  converted into a boolean results in `nil`.
+
+  see
+  - <https://www.w3.org/TR/xpath-31/#id-ebv>
+  - <https://www.w3.org/TR/sparql11-query/#ebv>
+
+  """
+  def ebv(value)
+
+  def ebv(true), do: XSD.Boolean.Value.true()
+  def ebv(false), do: XSD.Boolean.Value.false()
+
+  def ebv(%__MODULE__{value: nil}), do: XSD.Boolean.Value.false()
+  def ebv(%__MODULE__{} = value), do: value
+
+  def ebv(%XSD.String{} = string) do
+    if String.length(string.value) == 0,
+      do: XSD.Boolean.Value.false(),
+      else: XSD.Boolean.Value.true()
+  end
+
+  def ebv(%datatype{} = value) do
+    if XSD.Numeric.datatype?(datatype) do
+      if XSD.Value.valid?(value) and
+           not (value.value == 0 or value.value == :nan),
+         do: XSD.Boolean.Value.true(),
+         else: XSD.Boolean.Value.false()
+    end
+  end
+
+  def ebv(value) when is_binary(value) or is_number(value) do
+    value |> XSD.Value.coerce() |> ebv()
+  end
+
+  def ebv(_), do: nil
+
+  @doc """
+  Alias for `ebv/1`.
+  """
+  def effective(value), do: ebv(value)
+
+  @doc """
+  Returns `XSD.true` if the effective boolean value of the given argument is `XSD.false`, or `XSD.false` if it is `XSD.true`.
+
+  Otherwise it returns `nil`.
+
+  ## Examples
+
+      iex> XSD.Boolean.fn_not(XSD.true)
+      XSD.false
+      iex> XSD.Boolean.fn_not(XSD.false)
+      XSD.true
+
+      iex> XSD.Boolean.fn_not(true)
+      XSD.false
+      iex> XSD.Boolean.fn_not(false)
+      XSD.true
+
+      iex> XSD.Boolean.fn_not(42)
+      XSD.false
+      iex> XSD.Boolean.fn_not("")
+      XSD.true
+
+      iex> XSD.Boolean.fn_not(nil)
+      nil
+
+  see <https://www.w3.org/TR/xpath-functions/#func-not>
+  """
+  def fn_not(value) do
+    case ebv(value) do
+      %__MODULE__{value: true} -> XSD.Boolean.Value.false()
+      %__MODULE__{value: false} -> XSD.Boolean.Value.true()
+      nil -> nil
+    end
+  end
+
+  @doc """
+  Returns the logical `AND` of the effective boolean value of the given arguments.
+
+  It returns `nil` if only one argument is `nil` and the other argument is
+  `XSD.true` and `XSD.false` if the other argument is `XSD.false`.
+
+  ## Examples
+
+      iex> XSD.Boolean.logical_and(XSD.true, XSD.true)
+      XSD.true
+      iex> XSD.Boolean.logical_and(XSD.true, XSD.false)
+      XSD.false
+
+      iex> XSD.Boolean.logical_and(XSD.true, nil)
+      nil
+      iex> XSD.Boolean.logical_and(nil, XSD.false)
+      XSD.false
+      iex> XSD.Boolean.logical_and(nil, nil)
+      nil
+
+  see <https://www.w3.org/TR/sparql11-query/#func-logical-and>
+
+  """
+  def logical_and(left, right) do
+    case ebv(left) do
+      %__MODULE__{value: false} ->
+        XSD.Boolean.Value.false()
+
+      %__MODULE__{value: true} ->
+        case ebv(right) do
+          %__MODULE__{value: true} -> XSD.Boolean.Value.true()
+          %__MODULE__{value: false} -> XSD.Boolean.Value.false()
+          nil -> nil
+        end
+
+      nil ->
+        if match?(%__MODULE__{value: false}, ebv(right)) do
+          XSD.Boolean.Value.false()
+        end
+    end
+  end
+
+  @doc """
+  Returns the logical `OR` of the effective boolean value of the given arguments.
+
+  It returns `nil` if only one argument is `nil` and the other argument is
+  `XSD.false` and `XSD.true` if the other argument is `XSD.true`.
+
+  ## Examples
+
+      iex> XSD.Boolean.logical_or(XSD.true, XSD.false)
+      XSD.true
+      iex> XSD.Boolean.logical_or(XSD.false, XSD.false)
+      XSD.false
+
+      iex> XSD.Boolean.logical_or(XSD.true, nil)
+      XSD.true
+      iex> XSD.Boolean.logical_or(nil, XSD.false)
+      nil
+      iex> XSD.Boolean.logical_or(nil, nil)
+      nil
+
+  see <https://www.w3.org/TR/sparql11-query/#func-logical-or>
+
+  """
+  def logical_or(left, right) do
+    case ebv(left) do
+      %__MODULE__{value: true} ->
+        XSD.Boolean.Value.true()
+
+      %__MODULE__{value: false} ->
+        case ebv(right) do
+          %__MODULE__{value: true} -> XSD.Boolean.Value.true()
+          %__MODULE__{value: false} -> XSD.Boolean.Value.false()
+          nil -> nil
+        end
+
+      nil ->
+        if match?(%__MODULE__{value: true}, ebv(right)) do
+          XSD.Boolean.Value.true()
+        end
+    end
+  end
 end
 
 defmodule XSD.Boolean.Value do
--- test/unit/datatypes/boolean_test.exs
+++ test/unit/datatypes/boolean_test.exs
@@ -112,4 +112,133 @@ defmodule XSD.BooleanTest do
       end)
     end
   end
+
+  describe "ebv/1" do
+    import XSD.Boolean, only: [ebv: 1]
+
+    test "if the argument is a xsd:boolean typed literal and it has a valid lexical form, the EBV is the value of that argument" do
+      [
+        XSD.true(),
+        XSD.false(),
+        XSD.boolean(1),
+        XSD.boolean("0")
+      ]
+      |> Enum.each(fn value ->
+        assert ebv(value) == value
+      end)
+    end
+
+    test "any literal whose type is xsd:boolean or numeric is false if the lexical form is not valid for that datatype" do
+      [
+        XSD.boolean(42),
+        XSD.integer(3.14),
+        XSD.double("Foo")
+      ]
+      |> Enum.each(fn value ->
+        assert ebv(value) == XSD.false()
+      end)
+    end
+
+    test "if the argument is a xsd:string, the EBV is false if the operand value has zero length" do
+      assert ebv(XSD.string("")) == XSD.false()
+    end
+
+    test "if the argument is a xsd:string, the EBV is true if the operand value has length greater zero" do
+      assert ebv(XSD.string("bar")) == XSD.true()
+    end
+
+    test "if the argument is a numeric type with a valid lexical form having the value NaN or being numerically equal to zero, the EBV is false" do
+      [
+        XSD.integer(0),
+        XSD.integer("0"),
+        XSD.double("0"),
+        XSD.double("0.0"),
+        XSD.double(:nan),
+        XSD.double("NaN")
+      ]
+      |> Enum.each(fn value ->
+        assert ebv(value) == XSD.false()
+      end)
+    end
+
+    test "if the argument is a numeric type with a valid lexical form being numerically unequal to zero, the EBV is true" do
+      assert ebv(XSD.integer(42)) == XSD.true()
+      assert ebv(XSD.integer("42")) == XSD.true()
+      assert ebv(XSD.double("3.14")) == XSD.true()
+    end
+
+    test "Elixirs booleans are treated as XSD.Booleans" do
+      assert ebv(true) == XSD.true()
+      assert ebv(false) == XSD.false()
+    end
+
+    test "Elixirs strings are treated as XSD.strings" do
+      assert ebv("") == XSD.false()
+      assert ebv("foo") == XSD.true()
+      assert ebv("0") == XSD.true()
+    end
+
+    test "Elixirs numbers are treated as XSD.Numerics" do
+      assert ebv(0) == XSD.false()
+      assert ebv(0.0) == XSD.false()
+
+      assert ebv(42) == XSD.true()
+      assert ebv(3.14) == XSD.true()
+    end
+
+    test "all other arguments, produce nil" do
+      [
+        XSD.date("2010-01-01"),
+        XSD.time("00:00:00"),
+        nil,
+        self(),
+        [true],
+        {true},
+        %{foo: :bar}
+      ]
+      |> Enum.each(fn value ->
+        assert ebv(value) == nil
+      end)
+    end
+  end
+
+  test "truth-table of logical_and" do
+    [
+      {XSD.true(), XSD.true(), XSD.true()},
+      {XSD.true(), XSD.false(), XSD.false()},
+      {XSD.false(), XSD.true(), XSD.false()},
+      {XSD.false(), XSD.false(), XSD.false()},
+      {XSD.true(), nil, nil},
+      {nil, XSD.true(), nil},
+      {XSD.false(), nil, XSD.false()},
+      {nil, XSD.false(), XSD.false()},
+      {nil, nil, nil}
+    ]
+    |> Enum.each(fn {left, right, result} ->
+      assert XSD.Boolean.logical_and(left, right) == result,
+             "expected logical_and(#{inspect(left)}, #{inspect(right)}) to be #{inspect(result)}, but got #{
+               inspect(XSD.Boolean.logical_and(left, right))
+             }"
+    end)
+  end
+
+  test "truth-table of logical_or" do
+    [
+      {XSD.true(), XSD.true(), XSD.true()},
+      {XSD.true(), XSD.false(), XSD.true()},
+      {XSD.false(), XSD.true(), XSD.true()},
+      {XSD.false(), XSD.false(), XSD.false()},
+      {XSD.true(), nil, XSD.true()},
+      {nil, XSD.true(), XSD.true()},
+      {XSD.false(), nil, nil},
+      {nil, XSD.false(), nil},
+      {nil, nil, nil}
+    ]
+    |> Enum.each(fn {left, right, result} ->
+      assert XSD.Boolean.logical_or(left, right) == result,
+             "expected logical_or(#{inspect(left)}, #{inspect(right)}) to be #{inspect(result)}, but got #{
+               inspect(XSD.Boolean.logical_and(left, right))
+             }"
+    end)
+  end
 end
